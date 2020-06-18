68be7cb94417aff7f2a7f70312cc05623b39f92b
karan chanda <KaranChandra>
2020-06-18 16:09:50 +0530

    Updates

---
+++ precache-manifest.b609842d5b3622cfe3f483aef96c8a5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6a54708f532bb02491fd9e891c8a1f",
+    "url": "/reactTodo/index.html"
+  },
+  {
+    "revision": "68c949e58fb2fb5b3a1a",
+    "url": "/reactTodo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/reactTodo/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTodo/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c949e58fb2fb5b3a1a",
+    "url": "/reactTodo/static/js/main.a2b4767e.chunk.js"
+  },
+  {
+    "revision": "2cc1ce62983e867ff218",
+    "url": "/reactTodo/static/js/runtime-main.fb8931c4.js"
+  },
+  {
+    "revision": "f455eb09eded2096dda99ce7b9e15af2",
+    "url": "/reactTodo/static/media/profile.f455eb09.jpg"
+  }
+]);
